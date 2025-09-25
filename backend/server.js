4ca40e33cd2d332cb0b33026616ea36fd788ebdb
kishan817835 <kishan817835@gmail.com>
2025-09-25 17:40:02 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(
       "http://localhost:5173",
       "http://127.0.0.1:5173",
       "http://127.0.0.1:5174",
-      "https://portfolio-three-jet-53.vercel.app/"
+      "https://portfolio-three-jet-53.vercel.app"
     ],
     credentials: true,
   })
